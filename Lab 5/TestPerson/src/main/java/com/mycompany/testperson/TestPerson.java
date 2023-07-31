5d329f3fbead6d618132478a311a878244da61b9
TharinduBoo <141040115+TharinduBoo@users.noreply.github.com>
2023-07-31 19:19:22 +0530

    Update TestPerson.java

--- Lab 5/TestPerson/src/main/java/com/mycompany/testperson/TestPerson.java
+++ Lab 5/TestPerson/src/main/java/com/mycompany/testperson/TestPerson.java
@@ -3,13 +3,13 @@ package com.mycompany.testperson;
 public class TestPerson {
     public static void main(String[] args) {
         // Create a Student object
-        Student student = new Student("Gayantha Nadeemal", 28202, "Computer Science");
+        Student student = new Student("Tharindu Saratha", 29199, "Software e");
         System.out.println("Student Name: " + student.getName());
         System.out.println("Student ID: " + student.getID());
         System.out.println("Course: " + student.getCourse());
 
         // Create a Lecturer object
-        Lecturer lecturer = new Lecturer("Nadeemal Gayantha", 29222, "Software Engineering");
+        Lecturer lecturer = new Lecturer("Saratha jayakodi", 29522, "Software Engineering");
         System.out.println("\nLecturer Name: " + lecturer.getName());
         System.out.println("Lecturer ID: " + lecturer.getID());
         System.out.println("Programme: " + lecturer.getProgramme());
